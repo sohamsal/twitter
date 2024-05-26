ea3d5657f7e147bb25490312cf1e6ee0384b9ee1
soham <sohamsalunke10@gmail.com>
2024-05-25 22:54:16 -0400

    added redirect functionality, changed how delete works

--- components/Tweets.tsx
+++ components/Tweets.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { createClient } from "@/utils/supabase/client";
+import { tweetId } from "@/components/actions/action";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -57,34 +58,34 @@ export default function Tweets() {
         return (formatDate(tweet.created_at));
     };
 
-    const deleteTweet = async (tweet_id: number, who_posted: any) => {
-        const username = await fetchUser()
-        if (username && (who_posted.user.img == username.user_metadata.avatar_url)) {
-            console.log(username)
-            const { error } = await supabase.from(table).delete().eq('tweet_id', tweet_id);
+    // const deleteTweet = async (tweet_id: number, who_posted: any) => {
+    //     const username = await fetchUser()
+    //     if (username && (who_posted.user.img == username.user_metadata.avatar_url)) {
+    //         console.log(username)
+    //         const { error } = await supabase.from(table).delete().eq('tweet_id', tweet_id);
 
-            if (error) {
-                console.error('Error deleting tweet:', error);
-            } else {
-                console.log('Deleted tweet:', tweet_id);
-                fetchData();
-            }
-        } else {
-            alert("bros tryna delete someone else's tweet")
-        }
-    };
+    //         if (error) {
+    //             console.error('Error deleting tweet:', error);
+    //         } else {
+    //             console.log('Deleted tweet:', tweet_id);
+    //             fetchData();
+    //         }
+    //     } else {
+    //         alert("bros tryna delete someone else's tweet")
+    //     }
+    // };
+    
 
     return (
         <div>
             {tweetData.length > 0 ? (
                 <div className="flex-col justify-center dark:[color-scheme:dark]">
                     {tweetData.map(tweet => (
-                        <div className="p-4 w-96 bg-slate-900 mb-6" key={tweet.tweet_id}>
+                        <div className="p-4 w-96 bg-slate-900 mb-6 cursor-pointer" key={tweet.tweet_id} onClick={() => tweetId(tweet.tweet_id)}>
                             <img src={tweet.who_posted.user.img} alt="User Profile" className="rounded-full h-8 w-8" />
                             <p className="font-bold mt-1">{tweet.who_posted.user.name}</p>
                             <p>{tweet.tweet_content}</p>
                             <p className="text-gray-500 text-sm">{readableDate(tweet)}</p>
-                            <button className="bg-red" onClick={() => deleteTweet(tweet.tweet_id, tweet.who_posted)}>Delete</button>
                         </div>
                     ))}
                 </div>
---
+++ components/actions/action.ts
@@ -0,0 +1,7 @@
+'use server'
+ 
+import { redirect } from 'next/navigation'
+ 
+export async function tweetId(tweet_id: number) {
+  redirect(`/tweets/${tweet_id}`)
+}
