396026912d3deb7a559742b3762d5470ec0bdf68
soham <sohamsalunke10@gmail.com>
2024-05-27 01:30:03 -0400

    fixed favicon FINALLY (hopefully?)

--- middleware.ts
+++ middleware.ts
@@ -11,10 +11,11 @@ export const config = {
      * Match all request paths except:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - favicon.ico (root favicon file)
+     * - app/favicon (custom favicon path)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|app/favicon|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
