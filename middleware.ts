e9102d25918dbca2f13320522a751c0bfbd3369c
soham <sohamsalunke10@gmail.com>
2024-05-27 01:34:00 -0400

    please work

--- middleware.ts
+++ middleware.ts
@@ -12,10 +12,10 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (root favicon file)
-     * - app/favicon (custom favicon path)
+     * - app/favicon/favicon.ico (custom favicon path)
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|app/favicon|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|app/favicon/favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
